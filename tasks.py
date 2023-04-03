4442041aaf5577c660d0b1024a3bd2c7d8465f9f
Suite Royale <40777658+pnair20@users.noreply.github.com>
2023-04-03 19:43:33 +1000

    a

--- tasks.py
+++ tasks.py
@@ -23,12 +23,24 @@ def async_recommendation(user_email, *args, **kwargs):
 def recommendation_function(playlist_id, rec_playlist_id, spotify_token, spotify_username, ratings):
     sp = spotipy.Spotify(auth=spotify_token)
     playlist = sp.playlist(playlist_id)
-    tracks = playlist['tracks']['items']
+    
+    # Use a generator to stream the tracks in batches
+    tracks = stream_tracks(playlist['tracks'], sp, batch_size=50)
+    
+    valid_ratings = {}
+    audio_features = []
+    for batch in tracks:
+        # Only get audio features for tracks that have been rated
+        batch_valid_keys = [t['track']['id'] for t in batch if t['track']['id'] in ratings]
+        batch_audio_features = [feature for feature in sp.audio_features(batch_valid_keys) if feature is not None]
+        batch_valid_ratings = {key: ratings[key] for key in batch_valid_keys}
 
-    audio_features = [feature for feature in sp.audio_features(list(ratings.keys())) if feature is not None]
+        for feature in batch_audio_features:
+            valid_ratings[feature['id']] = batch_valid_ratings[feature['id']]
+            audio_features.append(feature)
 
-    valid_keys = [feature['id'] for feature in audio_features]
-    valid_ratings = {key: ratings[key] for key in valid_keys}
+    if len(audio_features) <= 1:
+        return None
 
     playlist_df = pd.DataFrame(audio_features)
     playlist_df['ratings'] = list(valid_ratings.values())
@@ -37,12 +49,7 @@ def recommendation_function(playlist_id, rec_playlist_id, spotify_token, spotify
                      "liveness", "loudness", "mode", "speechiness", "tempo", "valence"]]
     y = playlist_df['ratings']
 
-    if len(X) <= 1:
-        return None
-
     scaler = MinMaxScaler()
-    X_scaled = scaler.fit_transform(X)
-
     knn = KNeighborsClassifier()
     max_neighbors = min(30, len(X) - 1)
 
@@ -63,6 +70,9 @@ def recommendation_function(playlist_id, rec_playlist_id, spotify_token, spotify
     grid_search = GridSearchCV(estimator=knn, param_grid=param_grid, cv=cv, n_jobs=-1)
 
     try:
+        X_scaled = np.empty_like(X)
+        for i in range(0, len(X), 1000):
+            X_scaled[i:i+1000] = scaler.fit_transform(X[i:i+1000])
         grid_search.fit(X_scaled, y)
     except Exception as e:
         return None
@@ -72,15 +82,16 @@ def recommendation_function(playlist_id, rec_playlist_id, spotify_token, spotify
     rec_track_ids = set()
     recommendation_limit = ceil(len(playlist_df) / 2)
 
-    for track_id in playlist_df['id'].tolist():
+    for track in stream_tracks(playlist['tracks'], sp, batch_size=50):
+        track_id = track['track']['id']
         try:
             rec_tracks = sp.recommendations(seed_tracks=[track_id], limit=recommendation_limit)['tracks']
-            for track in rec_tracks:
-                rec_track_ids.add(track['id'])
+            for rec_track in rec_tracks:
+                rec_track_ids.add(rec_track['id'])
         except Exception as e:
-            return None
+            continue
 
-    track_chunks = [rec_track_ids[i:i + 100] for i in range(0, len(rec_track_ids), 100)]
+    track_chunks = [list(rec_track_ids)[i:i+100] for i in range(0, len(rec_track_ids), 100)]
 
     for track_chunk in track_chunks:
         sp.user_playlist_add_tracks(user=spotify_username, playlist_id=rec_playlist_id, tracks=track_chunk)
