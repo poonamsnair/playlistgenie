0cd2b93fa85d53421b4d6653ca5c2061aef1f74d
Suite Royale <40777658+pnair20@users.noreply.github.com>
2023-04-05 10:52:48 +1000

    a

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ socketio = SocketIO(app)
 
 
 
-SCOPE = 'user-library-read playlist-modify-public playlist-read-private'
+SCOPE = 'user-library-read playlist-modify-public playlist-read-private streaming'
 SPOTIPY_REDIRECT_URI = os.environ.get('SPOTIPY_REDIRECT_URI')
 SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
