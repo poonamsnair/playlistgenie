af4c5c27b710899d5ae4171a22b7711627ee5164
Suite Royale <40777658+pnair20@users.noreply.github.com>
2023-04-05 17:56:12 +1000

    a

--- app.py
+++ app.py
@@ -146,8 +146,10 @@ def index():
 
     auth_manager = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,
                                 redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE)
-    auth_url = auth_manager.get_authorize_url(show_dialog=True)
+    auth_url = auth_manager.get_authorize_url()
+    auth_url += "&show_dialog=true"  # Add the show_dialog parameter to the URL
     return render_template('index.html', auth_url=auth_url)
+
 
 @app.route('/logout')
 def logout():
